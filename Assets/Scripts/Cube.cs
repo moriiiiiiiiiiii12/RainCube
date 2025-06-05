5c1b8f491d151d973ec027b826e502b361a555f2
moriiiiiiiiiiii12 <moriiiiiiiiiiii12@gmail.com>
2025-06-05 14:47:53 +0300

    update

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.ComponentModel;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
 
